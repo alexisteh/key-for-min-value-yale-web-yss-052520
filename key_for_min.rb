ecef014a4a06f1c445247c0789d903481ead78a5
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-14 03:24:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  maxvee = 0 
+  minvee = 100 
   kookoo = nil 
   name_hash.each do |key,val| 
     if val < maxvee 
