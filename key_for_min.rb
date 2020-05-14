da02b258c8bd33778ea1870291bff57b2198929a
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-14 03:24:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   kookoo = nil 
   name_hash.each do |key,val| 
     if val < maxvee 
-      maxvee = val 
+      minvee = val 
       kookoo = key 
     end 
   end 
